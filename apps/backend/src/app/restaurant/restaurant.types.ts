3572a80d61351a5e6cfbf0ac596e2236068f8cb0
Keerati Limkulphong <imkrish.developer@gmail.com>
2019-05-21 21:46:56 +1200

    Make restaurant class to be GraphQL type

--- apps/backend/src/app/restaurant/restaurant.types.ts
+++ apps/backend/src/app/restaurant/restaurant.types.ts
@@ -1,9 +1,16 @@
+import { Field, ID, ObjectType } from 'type-graphql';
+
+@ObjectType()
 export class Restaurant {
+  @Field(type => ID)
   id: string;
 
+  @Field()
   title: string;
 
+  @Field()
   creationDate: number;
 
+  @Field()
   done: boolean;
 }
